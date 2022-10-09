1074a0c15b8fa5b835487180005c18de851acac3
RenanMarx144 <renan.marx144@gmail.com>
2022-10-09 13:20:19 -0300

    teste2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 function Home() { 
     return(
-        <div>Home</div>
+        <div>Homer
+
+            <Contador/>
+        </div>
+        
     )
+    
  }
+
+ function Contador() {
+    const [contador, setContador] = useState(1);
+    function AddCount() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={AddCount}>Adicionar</button>
+        </div>
+    )
+}
 export default Home
